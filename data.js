b1f3987c63f04d08d8cff67b8e815a845f51feca
Quyenhm03 <nguyenquyen2603.hoangmai@gmail.com>
2024-05-12 15:22:32 +0700

    update

---
+++ data.js
@@ -0,0 +1,239 @@
+// var videos = [
+//     {
+//         'id' : 1,
+//         'name':'Top 5 Programming Languages to Learn in 2021 | Best Programming Languages to Learn',
+//         'img' : 'https://img.youtube.com/vi/PpXUTUXU7Qc/maxresdefault.jpg',
+//         'video' :'https://media.geeksforgeeks.org/wp-content/uploads/20231020155223/Full-Stack-Development-_-LIVE-Classes-_-GeeksforGeeks.mp4',
+//         'auth' : 'FutureCoders',
+//         'timeup' : '3 months ago',
+//         'views' : '10M Views',     
+//         'likes' : 1560,
+//         'deslikes' : 125,
+//         'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nunc orci, tincidunt nec turpis in, consectetur malesuada odio. Etiam maximus augue et porta interdum. Fusce rhoncus eros ipsum, ut mattis eros vestibulum a. Fusce non molestie leo, ut tempor nibh. Donec vestibulum pulvinar orci, vitae aliquam diam euismod vel. Donec mattis congue est ac tempus. Sed hendrerit dui id quam fringilla, convallis fermentum elit commodo. ',
+//     },
+//     {
+//         'id' : 2,
+//         'name': 'Build A Password Generator with React JS - Beginners Tutorial',
+//         'video' :'https://media.geeksforgeeks.org/wp-content/uploads/20231020155223/Full-Stack-Development-_-LIVE-Classes-_-GeeksforGeeks.mp4',
+//         'auth' : 'FutureCoders',
+//         'timeup' : '3 months ago',
+//         'views' : '10M Views',
+//         'likes' : 1560,
+//         'deslikes' : 125,
+//         'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nunc orci, tincidunt nec turpis in, consectetur malesuada odio. Etiam maximus augue et porta interdum. Fusce rhoncus eros ipsum, ut mattis eros vestibulum a. Fusce non molestie leo, ut tempor nibh. Donec vestibulum pulvinar orci, vitae aliquam diam euismod vel. Donec mattis congue est ac tempus. Sed hendrerit dui id quam fringilla, convallis fermentum elit commodo. ',
+//     },
+//     {
+//         'id' : 3,
+//         'name':'Bella Ciao Full Song | La Casa De Papel | Money Heist | Netflix India',
+//         'video' :'https://media.geeksforgeeks.org/wp-content/uploads/20231020155223/Full-Stack-Development-_-LIVE-Classes-_-GeeksforGeeks.mp4',
+//         'auth' : 'FutureCoders',
+//         'timeup' : '3 months ago',
+//         'views' : '10M Views',
+//         'likes' : 1560,
+//         'deslikes' : 125,
+//         'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nunc orci, tincidunt nec turpis in, consectetur malesuada odio. Etiam maximus augue et porta interdum. Fusce rhoncus eros ipsum, ut mattis eros vestibulum a. Fusce non molestie leo, ut tempor nibh. Donec vestibulum pulvinar orci, vitae aliquam diam euismod vel. Donec mattis congue est ac tempus. Sed hendrerit dui id quam fringilla, convallis fermentum elit commodo. ',
+//     },
+//     {
+//         'id' : 4,
+//         'name':`DON'T EVER GIVE UP - Elon Musk (Motivational Video)`,
+//         'video' :'https://media.geeksforgeeks.org/wp-content/uploads/20231020155223/Full-Stack-Development-_-LIVE-Classes-_-GeeksforGeeks.mp4',
+//         'auth' : 'FutureCoders',
+//         'timeup' : '3 months ago',
+//         'views' : '10M Views',
+//         'likes' : 1560,
+//         'deslikes' : 125,
+//         'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nunc orci, tincidunt nec turpis in, consectetur malesuada odio. Etiam maximus augue et porta interdum. Fusce rhoncus eros ipsum, ut mattis eros vestibulum a. Fusce non molestie leo, ut tempor nibh. Donec vestibulum pulvinar orci, vitae aliquam diam euismod vel. Donec mattis congue est ac tempus. Sed hendrerit dui id quam fringilla, convallis fermentum elit commodo. ',
+//     },
+//     {
+//         'id' : 5,
+//         'name':'Javascript Fundamentals',
+//         'video' :'https://media.geeksforgeeks.org/wp-content/uploads/20231020155223/Full-Stack-Development-_-LIVE-Classes-_-GeeksforGeeks.mp4',
+//         'auth' : 'FutureCoders',
+//         'timeup' : '3 months ago',
+//         'views' : '10M Views',
+//         'likes' : 1560,
+//         'deslikes' : 125,
+//         'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nunc orci, tincidunt nec turpis in, consectetur malesuada odio. Etiam maximus augue et porta interdum. Fusce rhoncus eros ipsum, ut mattis eros vestibulum a. Fusce non molestie leo, ut tempor nibh. Donec vestibulum pulvinar orci, vitae aliquam diam euismod vel. Donec mattis congue est ac tempus. Sed hendrerit dui id quam fringilla, convallis fermentum elit commodo. ',
+//     },
+//     {
+//         'id' : 6,
+//         'name':'Build 15 JavaScript Projects - Vanilla JavaScript Course',
+//         'video' :'https://media.geeksforgeeks.org/wp-content/uploads/20231020155223/Full-Stack-Development-_-LIVE-Classes-_-GeeksforGeeks.mp4',
+//         'auth' : 'FutureCoders',
+//         'timeup' : '3 months ago',
+//         'views' : '10M Views',
+//         'likes' : 1560,
+//         'deslikes' : 125,
+//         'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nunc orci, tincidunt nec turpis in, consectetur malesuada odio. Etiam maximus augue et porta interdum. Fusce rhoncus eros ipsum, ut mattis eros vestibulum a. Fusce non molestie leo, ut tempor nibh. Donec vestibulum pulvinar orci, vitae aliquam diam euismod vel. Donec mattis congue est ac tempus. Sed hendrerit dui id quam fringilla, convallis fermentum elit commodo. ',
+//     },
+//     {
+//         'id' : 7,
+//         'name':' Build Real Time Face Detection With JavaScript',
+//         'video' :'https://media.geeksforgeeks.org/wp-content/uploads/20231020155223/Full-Stack-Development-_-LIVE-Classes-_-GeeksforGeeks.mp4',
+//         'auth' : 'FutureCoders',
+//         'timeup' : '3 months ago',
+//         'views' : '10M Views',
+//         'likes' : 1560,
+//         'deslikes' : 125,
+//         'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nunc orci, tincidunt nec turpis in, consectetur malesuada odio. Etiam maximus augue et porta interdum. Fusce rhoncus eros ipsum, ut mattis eros vestibulum a. Fusce non molestie leo, ut tempor nibh. Donec vestibulum pulvinar orci, vitae aliquam diam euismod vel. Donec mattis congue est ac tempus. Sed hendrerit dui id quam fringilla, convallis fermentum elit commodo. ',
+//     },
+//     {
+//         'id' : 8,
+//         'name': 'AWS Basics for Beginners - Full Course',
+//         'video' :'https://media.geeksforgeeks.org/wp-content/uploads/20231020155223/Full-Stack-Development-_-LIVE-Classes-_-GeeksforGeeks.mp4',
+//         'auth' : 'FutureCoders',
+//         'timeup' : '3 months ago',
+//         'views' : '10M Views',
+//         'likes' : 1560,
+//         'deslikes' : 125,
+//         'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nunc orci, tincidunt nec turpis in, consectetur malesuada odio. Etiam maximus augue et porta interdum. Fusce rhoncus eros ipsum, ut mattis eros vestibulum a. Fusce non molestie leo, ut tempor nibh. Donec vestibulum pulvinar orci, vitae aliquam diam euismod vel. Donec mattis congue est ac tempus. Sed hendrerit dui id quam fringilla, convallis fermentum elit commodo. ',
+//     },
+// ]
+
+// const videosContainer = document.querySelector('.videos__container');
+// const videoElements = Array.from(videosContainer.querySelectorAll('.video'));
+
+// const videos = videoElements.map((videoElement) => {
+//   const thumbnail = videoElement.querySelector('.video__thumbnail img').src;
+//   const videoUrl = videoElement.querySelector('.video__thumbnail a').href;
+//   const title = videoElement.querySelector('.title h3').textContent;
+//   const channel = videoElement.querySelector('.title a').textContent;
+//   const views = videoElement.querySelector('.title span').textContent;
+
+//   return {
+//     thumbnail,
+//     videoUrl,
+//     title,
+//     channel,
+//     views
+//   };
+// });
+
+// Assuming you have an array of video data called "videos"
+const videos = [
+    {
+      thumbnail: "https://img.youtube.com/vi/PpXUTUXU7Qc/maxresdefault.jpg",
+      videoLink: "playvideo.html",
+      channelIcon: "http://aninex.com/images/srvc/web_de_icon.png",
+      title: "Top 5 Programming Languages to Learn in 2021 | Best Programming Languages to Learn",
+      channelName: "FutureCoders",
+      views: "10M Views • 3 Months Ago"
+    },
+    {
+      thumbnail: "https://img.youtube.com/vi/PpXUTUXU7Qc/maxresdefault.jpg",
+      videoLink: "playvideo.html",
+      channelIcon: "http://aninex.com/images/srvc/web_de_icon.png",
+      title: "Top 5 Programming Languages to Learn in 2021 | Best Programming Languages to Learn",
+      channelName: "FutureCoders",
+      views: "10M Views • 3 Months Ago"
+    },
+    {
+      thumbnail: "https://img.youtube.com/vi/PpXUTUXU7Qc/maxresdefault.jpg",
+      videoLink: "playvideo.html",
+      channelIcon: "http://aninex.com/images/srvc/web_de_icon.png",
+      title: "Top 5 Programming Languages to Learn in 2021 | Best Programming Languages to Learn",
+      channelName: "FutureCoders",
+      views: "10M Views • 3 Months Ago"
+    },
+    {
+      thumbnail: "https://img.youtube.com/vi/PpXUTUXU7Qc/maxresdefault.jpg",
+      videoLink: "playvideo.html",
+      channelIcon: "http://aninex.com/images/srvc/web_de_icon.png",
+      title: "Top 5 Programming Languages to Learn in 2021 | Best Programming Languages to Learn",
+      channelName: "FutureCoders",
+      views: "10M Views • 3 Months Ago"
+    },
+    {
+      thumbnail: "https://img.youtube.com/vi/PpXUTUXU7Qc/maxresdefault.jpg",
+      videoLink: "playvideo.html",
+      channelIcon: "http://aninex.com/images/srvc/web_de_icon.png",
+      title: "Top 5 Programming Languages to Learn in 2021 | Best Programming Languages to Learn",
+      channelName: "FutureCoders",
+      views: "10M Views • 3 Months Ago"
+    },
+    {
+      thumbnail: "https://img.youtube.com/vi/PpXUTUXU7Qc/maxresdefault.jpg",
+      videoLink: "playvideo.html",
+      channelIcon: "http://aninex.com/images/srvc/web_de_icon.png",
+      title: "Top 5 Programming Languages to Learn in 2021 | Best Programming Languages to Learn",
+      channelName: "FutureCoders",
+      views: "10M Views • 3 Months Ago"
+    },
+    {
+      thumbnail: "https://img.youtube.com/vi/PpXUTUXU7Qc/maxresdefault.jpg",
+      videoLink: "playvideo.html",
+      channelIcon: "http://aninex.com/images/srvc/web_de_icon.png",
+      title: "Top 5 Programming Languages to Learn in 2021 | Best Programming Languages to Learn",
+      channelName: "FutureCoders",
+      views: "10M Views • 3 Months Ago"
+    },
+    {
+      thumbnail: "https://img.youtube.com/vi/PpXUTUXU7Qc/maxresdefault.jpg",
+      videoLink: "playvideo.html",
+      channelIcon: "http://aninex.com/images/srvc/web_de_icon.png",
+      title: "Top 5 Programming Languages to Learn in 2021 | Best Programming Languages to Learn",
+      channelName: "FutureCoders",
+      views: "10M Views • 3 Months Ago"
+    },
+  ];
+  
+  // Get the container element
+  const videosContainer = document.querySelector(".videos__container");
+  
+  // Iterate through the videos array and generate the video elements
+  videos.forEach((video) => {
+    // Create the video element
+    const videoElement = document.createElement("div");
+    videoElement.classList.add("video");
+  
+    // Create the video thumbnail element
+    const thumbnailElement = document.createElement("div");
+    thumbnailElement.classList.add("video__thumbnail");
+    const thumbnailImageElement = document.createElement("img");
+    thumbnailImageElement.src = video.thumbnail;
+    thumbnailImageElement.alt = "";
+    thumbnailElement.appendChild(thumbnailImageElement);
+  
+    // Create the link to play the video
+    const playLinkElement = document.createElement("a");
+    playLinkElement.href = video.videoLink;
+    const playIconElement = document.createElement("img");
+    playIconElement.src = "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/395_Youtube_logo-512.png";
+    playIconElement.alt = "";
+    playLinkElement.appendChild(playIconElement);
+    thumbnailElement.appendChild(playLinkElement);
+  
+    // Create the video details element
+    const detailsElement = document.createElement("div");
+    detailsElement.classList.add("video__details");
+  
+    // Create the author element
+    const authorElement = document.createElement("div");
+    const channelIconElement = document.createElement("img");
+    channelIconElement.src = video.channelIcon;
+    channelIconElement.alt = "";
+    authorElement.appendChild(channelIconElement);
+  
+    // Create the title element
+    const titleElement = document.createElement("div");
+    titleElement.classList.add("title");
+    const titleHeadingElement = document.createElement("h3");
+    titleHeadingElement.textContent = video.title;
+    const channelLinkElement = document.createElement("a");
+    channelLinkElement.href = "";
+    channelLinkElement.textContent = video.channelName;
+    const viewsElement = document.createElement("span");
+    viewsElement.textContent = video.views;
+  
+    titleElement.appendChild(titleHeadingElement);
+    titleElement.appendChild(channelLinkElement);
+    titleElement.appendChild(viewsElement);
+  
+    detailsElement.appendChild(authorElement);
+    detailsElement.appendChild(titleElement);
+  
+    videoElement.appendChild(thumbnailElement);
+    videoElement.appendChild(detailsElement);
+  
+    videosContainer.appendChild(videoElement);
+  });
